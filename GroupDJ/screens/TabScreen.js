ae76c22cbfb736cfca0b1d532e7dd420d3ee46f6
jautrey <jautrey@mail.csuchico.edu>
2020-03-03 12:44:55 -0800

    Adds tabscreen.js

---
+++ GroupDJ/screens/TabScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View } from 'react-native';
+import PlayerControls from '../components/PlayerControls.js'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import RoomOwnerScreen from '../screens/RoomOwnerScreen.js'
+import SearchScreen from '../screens/SearchScreen.js'
+
+const Tab = createBottomTabNavigator();
+
+const TabScreen = ({ navigation }) => {
+  return(
+    <View style={{ flex: 1, display: 'flex', justifyContent: 'center'}}>
+      <Tab.Navigator
+      initialRouteName="Now Playing"
+      tabBarOptions={{
+        activeBackgroundColor: '#222',
+        activeTintColor: '#fff',
+        inactiveTintColor: '#888',
+        inactiveBackgroundColor: '#222',
+        style: {
+          backgroundColor: '#222'
+        },
+      }}
+      >
+        <Tab.Screen name="Now Playing" component={RoomOwnerScreen} />
+        <Tab.Screen name="Search" component={SearchScreen} />
+      </Tab.Navigator>
+    </View>
+  );
+}
+
+export default TabScreen;
