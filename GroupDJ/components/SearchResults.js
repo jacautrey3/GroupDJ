8b1f6a48ff2cfdb95bea1ddbfd020eb3b07f7732
jautrey <jautrey@mail.csuchico.edu>
2020-04-04 23:41:17 -0700

    fixes bug with artist not having a picture

--- GroupDJ/components/SearchResults.js
+++ GroupDJ/components/SearchResults.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import { TextInput, View, FlatList, StyleSheet, Text } from 'react-native';
 import { SearchBar, ListItem } from 'react-native-elements';
 import { SpotifyWebApi } from './Home.js'
-import { PlaySong } from '../spotifyFunctions.js'
+import { PlaySong, findDevices } from '../spotifyFunctions.js'
+var firebase = require("firebase");
 
 export default class SearchResults extends Component {
   constructor(props) {
     super(props);
   this.state = {
-    results: null
+    results: null,
+    devices: []
   };
+  this.findDevices = findDevices.bind(this);
 }
 
   componentDidUpdate(prevProps) {
@@ -19,7 +22,13 @@ export default class SearchResults extends Component {
   }
 
   componentDidMount() {
-    this.suggestionSelected(this.props.itemSelected, this.props.itemSelected.type)
+    this.findDevices();
+    this.suggestionSelected(this.props.itemSelected, this.props.itemSelected.type);
+  }
+
+  addToQueue(item) {
+    var roomKey = global.roomKey;
+    var key = firebase.database().ref('/Rooms/'+roomKey+"/queue").push(item);
   }
 
   suggestionSelected (value, type) {
@@ -60,30 +69,35 @@ export default class SearchResults extends Component {
     <ListItem
     containerStyle={{backgroundColor: '#333'}}
     titleStyle={{color: '#fff'}}
-    button onPress={() => {PlaySong(item.id)}}
+    button onPress={() => {
+      this.addToQueue(item)
+      // PlaySong(item.id, this.state.devices)
+      console.log("RENDER ITEM", item)
+    }}
     title={item.name}
     bottomDivider
     />
 )
 
   renderTopTracks(item) {
+    console.log("LOGGING THE DATA", item);
     return (
-      <View>
-        <FlatList style = {styles.autocompleteContainer}
+      <View style={{flex:1, width: '100%'}}>
+        <FlatList
+        contentContainerStyle={{ flexGrow: 1 }}
+        style={{flex: 1, width: '100%'}}
         data={this.state.results}
         keyExtractor={item => item.id}
         renderItem={this.renderItem}
         extraData={this.state}
         />
-        <Text> {item.name}</Text>
       </View>
     );
-    console.log("LOGGING THE DATA", data);
   }
 
   render() {
     return (
-      <View>
+      <View style={{flex:1, width: '100%'}}>
         {this.renderTopTracks(this.props.itemSelected)}
       </View>
     );
@@ -93,7 +107,7 @@ export default class SearchResults extends Component {
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#000',
-    flex: 1,
+    flexGrow: 1,
     paddingTop: 30
   },
   autocompleteContainer: {
