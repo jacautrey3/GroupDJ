b65e52d4a9f75a525a858566c53e50341315e8a1
jautrey <jautrey@mail.csuchico.edu>
2020-04-05 18:14:12 -0700

    Adds Header to QueueScreen.js

--- GroupDJ/screens/QueueScreen.js
+++ GroupDJ/screens/QueueScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { Header } from 'react-native-elements';
 import Queue from '../components/Queue.js'
 import styles from '../style.js'
 
 const QueueScreen = ({ }) => {
   return(
     <View style={{ flex: 1, display: 'flex', justifyContent: 'center', backgroundColor: '#000'}}>
-      <Text style= {{color: 'white', textAlign: 'center', fontWeight: 'bold', fontSize: 20, marginTop: '10%', marginBottom: 20}}> Queue </Text>
+    <Header
+    containerStyle={{borderBottomWidth: 0}}
+    backgroundColor="#000"
+    centerComponent={{text: 'Queue', style: {color: 'white', fontSize: 20, fontWeight:'bold'}}}
+    />
       <Queue/>
     </View>
   );
